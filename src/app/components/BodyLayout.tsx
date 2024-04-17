4403b651470e2fbbc6b50051c460bbd90713f806
qqppgwa <qqppgwa@gmail.com>
2024-04-17 16:24:34 +0800

    feat: 🎸 Header title

--- src/app/components/BodyLayout.tsx
+++ src/app/components/BodyLayout.tsx
@@ -1,15 +1,19 @@
+"use client";
 import React, { ReactNode } from "react";
+import { usePathname } from "next/navigation";
 import Header from "./Header";
 
 interface LayoutProps {
-  title?: string;
   children: JSX.Element | ReactNode;
 }
 
-const Layout = ({ title, children }: LayoutProps) => {
+const Layout = ({ children }: LayoutProps) => {
+  const pathname = usePathname();
+  const headerTitle = pathname === "/" ? "home" : "Find the cheese";
+
   return (
     <div className="w-full">
-      <Header title={title} />
+      <Header title={headerTitle} />
       <main className="mt-[50px] bg-amber-50 p-4 min-h-screen min-w-[fit-content]">
         <div className="p-4 max-w-[720px] mx-auto bg-white rounded shadow md:w-auto">
           {children}
